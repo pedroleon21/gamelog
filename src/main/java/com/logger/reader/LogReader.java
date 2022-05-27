6fca25477f85d001bb48d89dd6c28631f4cd6545
Pedro Leon Paranayba Clerot <52296989+pedroleon21@users.noreply.github.com>
2022-05-26 23:00:33 -0300

    renomeando arquivo

--- src/main/java/com/logger/reader/LogReader.java
+++ src/main/java/com/logger/reader/LogReader.java
@@ -5,13 +5,21 @@ import org.eclipse.microprofile.config.inject.ConfigProperty;
 import javax.enterprise.context.RequestScoped;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
 
 @RequestScoped
 public class LoggerReader {
 
     @ConfigProperty(name = "log.file.path")
     String filePath;
-    public String readeFile() {
+    public List<String> readeFile() {
+        String file = read();
+        String[] eventos = file.split("\\n");
+        return Arrays.asList(eventos);
+    }
+
+    private String read() {
         try{
             return Files.readString(Path.of(filePath));
         }catch (Exception e){
