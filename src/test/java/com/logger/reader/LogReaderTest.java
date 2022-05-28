8a201bf6b3e1816f5b23729ccded0a383995dc62
Pedro Leon Paranayba Clerot <52296989+pedroleon21@users.noreply.github.com>
2022-05-27 23:17:11 -0300

    metodo para pegar resumo de game cru e compeltando teste de metodos de listagem e busca de games

--- src/test/java/com/logger/reader/LogReaderTest.java
+++ src/test/java/com/logger/reader/LogReaderTest.java
@@ -1,10 +1,12 @@
 package com.logger.reader;
 
+import com.logger.Parser;
+
 import java.util.List;
 
 public class LogReaderTest extends LogReader{
-    public LogReaderTest(String filePath) {
-        super(filePath);
+    public LogReaderTest(String filePath, Parser parser) {
+        super(filePath,parser);
     }
     public List<String> readFile(){
         return getAllEvents();
--- src/test/java/com/logger/reader/LogReaderTester.java
+++ src/test/java/com/logger/reader/LogReaderTester.java
@@ -1,18 +1,29 @@
 package com.logger.reader;
 
+import com.entities.Game;
+import com.logger.Parser;
 import org.junit.jupiter.api.*;
 
 import java.util.Hashtable;
 import java.util.List;
+import java.util.Objects;
+import java.util.Random;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class LogReaderTester {
     //TODO: colocar filepath num arquivo de propriedades
     String filePath = "games.log";
-    LogReaderTest logReader = new LogReaderTest(filePath);
+    LogReaderTest logReader = new LogReaderTest(filePath,new Parser());
     @Test
     void pegarTodosEventos(){
-        Hashtable<String, List<String>> eventos = logReader.readGames();
+        Hashtable<Integer, List<String>> eventos = logReader.readGames();
         assert (!eventos.isEmpty());
+    }
+    @Test
+    void pegarGame(){
+        for(int i =0; i < 20;i++){//existem 20 jogos
+            Game game = logReader.getGeme(i);
+            assert (Objects.nonNull(game));
+        }
     }
 }
