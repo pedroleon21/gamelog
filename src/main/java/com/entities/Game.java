327f074192adff77e7a020fd3e98e85ab31f5625
Pedro Leon Paranayba Clerot <52296989+pedroleon21@users.noreply.github.com>
2022-05-27 23:13:51 -0300

    game complento

--- src/main/java/com/entities/Game.java
+++ src/main/java/com/entities/Game.java
@@ -1,4 +1,40 @@
 package com.entities;
 
+import java.util.List;
+
 public class Game {
+    private int totalKills;
+    private List<Kill> kills;
+    private List<String> players;
+
+    public Game(int totalKills, List<Kill> kills, List<String> players) {
+        super();
+        this.totalKills = totalKills;
+        this.kills = kills;
+        this.players = players;
+    }
+
+    public int getTotalKills() {
+        return totalKills;
+    }
+
+    public void setTotalKills(int totalKills) {
+        this.totalKills = totalKills;
+    }
+
+    public List<Kill> getKills() {
+        return kills;
+    }
+
+    public void setKills(List<Kill> kills) {
+        this.kills = kills;
+    }
+
+    public List<String> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(List<String> players) {
+        this.players = players;
+    }
 }
