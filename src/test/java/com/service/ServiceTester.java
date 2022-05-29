35211a6e49e2663e0a63a0a75ef9b085de0e70f0
Pedro Leon Paranayba Clerot <52296989+pedroleon21@users.noreply.github.com>
2022-05-29 11:05:07 -0300

    testando metodos que não servem dado direto da dao

--- src/test/java/com/service/ServiceTester.java
+++ src/test/java/com/service/ServiceTester.java
@@ -1,8 +1,27 @@
 package com.service;
 
+import com.dao.Dao;
+import com.entries.Game;
+import com.entries.ResumeGame;
+import com.reader.LogReader;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.junit.jupiter.api.Test;
 
-public class ServiceTester {
+import java.util.Hashtable;
+import java.util.Objects;
+import java.util.Random;
 
-//    @Test
+public class ServiceTester {
+    Service service = new Service("games.log");
+
+    @Test
+    void getAllResumes(){
+        Hashtable<String, ResumeGame> tableResume = service.getAllResumes();
+        assert (!tableResume.isEmpty());
+    }
+    @Test
+    void pegarResumo(){
+        Game game = service.pegarGame(new Random().nextInt(21));
+        assert(Objects.nonNull(game));
+    }
 }
